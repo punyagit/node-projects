8c32e93db2d53e581b4ff88cad4339411709e258
punya <punyachapagain@gmail.com>
2018-10-14 21:53:41 +1100

    find word in file

--- word-count/wordCount.js
+++ word-count/wordCount.js
@@ -1,15 +1,34 @@
 var fs = require('fs');
 // reading file synchronomously
-var readMe = fs.readFileSync('file.txt', 'utf8');
-var wordsToFind = "Node";
+var readMe1 = fs.readFileSync('file.txt', 'utf8');
+var readMe = readMe1.toLowerCase()
+console.log(readMe)
+var wordsToFind = "node";
 
-if (readMe.indexOf(wordsToFind) >= 0) { 
+
+
+
+function countWord(word){
+  if (readMe.indexOf(word) >= 0) { 
     console.log("hey")
   } else { 
     console.log('No');
   }
+}
 
 
-
+function mult(file) {
+  //console.log(...args)
+  var sliceNum = file.indexOf(wordsToFind)
+  console.log(file.indexOf(wordsToFind))
+  if (file.indexOf(wordsToFind) < 0){ // it will stop for ulimited loop
+      console.log(file)
+      return file
+  }
+  //console.log("here is " + args)
+  return mult(file.slice(sliceNum + 1))
+}
 //console.log(readMe);
+mult(readMe)
+countWord(wordsToFind);
 
