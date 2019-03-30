73e471d4ecf59cdd0d94261e06b32976a5b3dbd7
punya <punyachapagain@gmail.com>
2019-03-30 12:41:45 +1100

    count word in a file

--- word-count/wordCount.js
+++ word-count/wordCount.js
@@ -2,28 +2,25 @@ var fs = require('fs');
 // reading file synchronomously
 var readFile = fs.readFileSync('file.txt', 'utf8');
 var readFileLowerCase = readFile.toLowerCase()
-var wordsToFind = "node";
+var wordsToFind = "javascript";
 
-function countWord(word){
-  if (readFileLowerCase.indexOf(word) >= 0) { 
+function countWord(word) {
+  if (readFileLowerCase.indexOf(word) >= 0) {
     console.log("I found the word")
-  } else { 
+  } else {
     console.log('No Word is Found');
   }
 }
-
+let count = 0
 function mult(file) {
-  //console.log(...args)
   var sliceNum = file.indexOf(wordsToFind)
-  console.log(file.indexOf(wordsToFind))
-  if (file.indexOf(wordsToFind) < 0){ // it will stop for ulimited loop
-      console.log(file)
-      return file
-  }
-  //console.log("here is " + args)
+  //console.log(sliceNum)
+  if (sliceNum < 0)
+    return count
+  count++
   return mult(file.slice(sliceNum + 1))
 }
 //console.log(readMe);
-mult(readFileLowerCase)
-countWord(wordsToFind);
-
+console.log(mult(readFileLowerCase))
+//countWord(wordsToFind);
+console.log("punya")
