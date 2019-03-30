e1a2e8e5853571bc91b09df60eba58aa2b7f4a59
punya <punyachapagain@gmail.com>
2019-03-30 20:47:27 +1100

    read file asynchronomous

--- word-count/wordCount.js
+++ word-count/wordCount.js
@@ -1,26 +1,47 @@
 var fs = require('fs');
 // reading file synchronomously
-var readFile = fs.readFileSync('file.txt', 'utf8');
-var readFileLowerCase = readFile.toLowerCase()
-var wordsToFind = "javascript";
+//var readFile = fs.readFileSync('file.txt', 'utf8');
 
-function countWord(word) {
+
+var wordsToFind = "node";
+let count = 0
+
+function countWordFile(callback) {
+  fs.readFile('file.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    var readFileLowerCase = data.toLowerCase()
+
+    return callback(readFileLowerCase)
+
+
+  });
+
+}
+
+
+
+
+function countWord(readFileLowerCase, word, callback) {
   if (readFileLowerCase.indexOf(word) >= 0) {
     console.log("I found the word")
   } else {
     console.log('No Word is Found');
   }
+  callback(readFileLowerCase)
 }
-let count = 0
+
+
+
 function mult(file) {
   var sliceNum = file.indexOf(wordsToFind)
-  //console.log(sliceNum)
+  console.log(sliceNum)
   if (sliceNum < 0)
+
     return count
   count++
   return mult(file.slice(sliceNum + 1))
 }
-//console.log(readMe);
-console.log(mult(readFileLowerCase))
-//countWord(wordsToFind);
+
+console.log(countWordFile(mult))
 console.log("punya")
+
