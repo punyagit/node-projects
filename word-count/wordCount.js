d22342e492d4548c5b8f8314bae5b1962c015272
punya <punyachapagain@gmail.com>
2019-03-31 20:26:49 +1100

    refactor

--- word-count/wordCount.js
+++ word-count/wordCount.js
@@ -1,7 +1,4 @@
 var fs = require('fs');
-// reading file synchronomously
-//var readFile = fs.readFileSync('file.txt', 'utf8');
-
 
 var wordsToFind = "node";
 let count = 0
@@ -12,31 +9,14 @@ function countWordFile(callback) {
     var readFileLowerCase = data.toLowerCase()
 
     return callback(readFileLowerCase)
-
-
   });
 
 }
 
-
-
-
-function countWord(readFileLowerCase, word, callback) {
-  if (readFileLowerCase.indexOf(word) >= 0) {
-    console.log("I found the word")
-  } else {
-    console.log('No Word is Found');
-  }
-  callback(readFileLowerCase)
-}
-
-
-
 function mult(file) {
   var sliceNum = file.indexOf(wordsToFind)
-  console.log(sliceNum)
+  //console.log(sliceNum)
   if (sliceNum < 0)
-
     return count
   count++
   return mult(file.slice(sliceNum + 1))
