383c9ff0c2324d1b45de5d12de68c194f7a6f659
punya <punyachapagain@gmail.com>
2019-05-29 20:45:08 +1000

    get multiple data from google api

--- google-map/index.js
+++ google-map/index.js
@@ -1,4 +1,50 @@
 let Config = require('../config/config.json')
+const https = require('http');
 
-fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=${Config.Key}`)
-    .then(data => console.log(data))
+const googleMapsClient = require('@google/maps').createClient({
+    key: Config.KEY,
+    Promise: Promise
+});
+
+let destinations = [
+    { origin: "glenfield, Nsw", destination: "liverpool, NSw" },
+    { origin: "Liverpool, Nsw", destination: "Rockdale, NSw" },
+    { origin: "kogarah, Nsw", destination: "clovelly, NSw" },
+]
+
+let promisesArray = destinations.map(destination => {
+    // make a new promise for each element of cities
+    return new Promise((resolve, reject) => {
+        googleMapsClient.directions(destination)
+            .asPromise()
+            .then(data => {
+                //console.log(data.json.routes[0].legs[0].distance)
+                //console.log(data.json.routes[0].legs[0].duration)
+                resolve(data.json.routes[0].legs[0].distance["value"])
+            }
+
+            )
+
+
+
+    })
+})
+
+Promise.all(promisesArray)
+    .then(function (results) {
+        console.log(typeof (results[0]))
+    })
+    .catch(function (error) {
+        console.log(error)
+    })
+
+
+// googleMapsClient.directions()
+//     .asPromise()
+//     .then(data => {
+//         console.log(data.json.routes[0].legs[0].distance)
+//         console.log(data.json.routes[0].legs[0].duration)
+//     }
+
+//     )
+
